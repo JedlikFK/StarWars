e459874c18dacb78df3db0242ee6dce1687d30de
JedlikFK <mobilephp@gmail.com>
2021-02-24 12:29:46 +0100

    commit

--- starwars.py
+++ starwars.py
@@ -1,1 +1,17 @@
-HELLO
+import turtle
+import random
+import time
+
+space = turtle.Screen()
+space.setup(width=800, height=600)
+space.bgpic("background.png")
+space.addshape("sprite.gif")
+
+
+urhajo= turtle.Turtle()
+urhajo.shape("sprite.gif")
+
+
+while True:
+    space.update()
+    time.sleep(0.2)
