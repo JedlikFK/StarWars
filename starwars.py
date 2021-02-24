e7b50d826ba7abae99deecf192b8682d9a7c80df
JedlikFK <mobilephp@gmail.com>
2021-02-24 13:02:23 +0100

    easy

--- starwars.py
+++ starwars.py
@@ -2,16 +2,56 @@ import turtle
 import random
 import time
 
+def fel():
+    ypozicio=urhajo.ycor()
+    ypozicio+=10
+    urhajo.sety(ypozicio)
+
+def le():
+    ypozicio=urhajo.ycor()
+    ypozicio-=10
+    urhajo.sety(ypozicio)
+
+def jobbra():
+    xpozicio=urhajo.xcor()
+    xpozicio+=10
+    urhajo.setx(xpozicio)
+
+def balra():
+    xpozicio=urhajo.xcor()
+    xpozicio-=10
+    urhajo.setx(xpozicio)
+
 space = turtle.Screen()
 space.setup(width=800, height=600)
 space.bgpic("background.png")
 space.addshape("sprite.gif")
-
+space.tracer(0)
+space.listen()
+space.onkey(fel ,"Up")
+space.onkey(le ,"Down")
+space.onkey(balra ,"Left")
+space.onkey(jobbra ,"Right")
 
 urhajo= turtle.Turtle()
 urhajo.shape("sprite.gif")
+urhajo.penup()
+
 
 
 while True:
+
+    if urhajo.ycor()>300:
+        urhajo.sety(-300)
+    
+    if urhajo.ycor()<-300:
+        urhajo.sety(300)
+     
+    if urhajo.xcor()>400:
+        urhajo.setx(-400)
+    
+    if urhajo.xcor()<-400:
+        urhajo.setx(400)
+
     space.update()
     time.sleep(0.2)
