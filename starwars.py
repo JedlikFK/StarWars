c84e2622360f1ccbcefb259cf5a3325f911b8acc
JedlikFK <mobilephp@gmail.com>
2021-03-16 12:45:51 +0100

    KÉSZEN VAN!

--- starwars.py
+++ starwars.py
@@ -1,5 +1,5 @@
 import turtle
-import random
+from random import randint 
 import time
 
 def fel():
@@ -22,10 +22,24 @@ def balra():
     xpozicio-=10
     urhajo.setx(xpozicio)
 
+kijelzo = turtle.Turtle()
+kijelzo.hideturtle()
+kijelzo.penup()
+kijelzo.goto(0,260) 
+kijelzo.color("white")
+
+eletkijelzo=turtle.Turtle()
+eletkijelzo.hideturtle()
+eletkijelzo.penup()
+eletkijelzo.goto(0,-260) 
+eletkijelzo.color("white")
+
+
 space = turtle.Screen()
 space.setup(width=800, height=600)
 space.bgpic("background.png")
 space.addshape("sprite.gif")
+space.addshape("meteor1.gif")
 space.tracer(0)
 space.listen()
 space.onkey(fel ,"Up")
@@ -37,23 +51,65 @@ urhajo= turtle.Turtle()
 urhajo.shape("sprite.gif")
 urhajo.penup()
 
+meteor1=turtle.Turtle()
+meteor1.shape("meteor1.gif")
+meteor1.penup()
+meteor1.setx(400)
+meteor1.sety(randint(-30,30)*10)
 
-
-
-
-while True:
-
-    if urhajo.ycor()>300:
-        urhajo.sety(-300)
+szamlalo=0
+eletszamlalo=1
+while eletszamlalo!=0:
     
-    if urhajo.ycor()<-300:
-        urhajo.sety(300)
-     
-    if urhajo.xcor()>400:
-        urhajo.setx(-400)
-    
-    if urhajo.xcor()<-400:
-        urhajo.setx(400)
 
-    space.update()
-    time.sleep(0.2)
+    while meteor1.xcor() > -400:
+        space.update()
+        time.sleep(0.05)
+        meteor_mozgas = meteor1.xcor()
+        meteor_mozgas -= 10
+        meteor1.setx(meteor_mozgas)
+
+        eletkijelzo.write(f"Életeid: {eletszamlalo}", align="center", font=("Arial", 30, "bold"))
+        
+        if meteor1.xcor()==urhajo.xcor() and meteor1.ycor()==urhajo.ycor():
+            eletkijelzo.clear()
+            eletszamlalo-=1
+            meteor1.setx(400)
+            meteor1.sety(randint(-30,30)*10)
+            
+
+        if eletszamlalo==0:
+            kijelzo.goto(0,0)
+            kijelzo.write("GAME OVER", align="center", font=("Arial", 60, "bold"))
+            urhajo.setx(10000)
+            urhajo.sety(10000)
+    
+    while meteor1.xcor() <= -400:
+        
+        if meteor1.xcor()==-400:
+            meteor1.setx(400)
+            meteor1.sety(randint(-30,30)*10)
+            if eletszamlalo!=0:
+                szamlalo+=1
+
+        kijelzo.clear()
+        kijelzo.write(f"Pontjaid: {szamlalo}", align="center", font=("Arial", 30, "bold"))
+        
+        if urhajo.ycor()>300:
+            urhajo.sety(-300)
+        
+        if urhajo.ycor()<-300:
+            urhajo.sety(300)
+        
+        if urhajo.xcor()>400:
+            urhajo.setx(-400)
+        
+        if urhajo.xcor()<-400:
+            urhajo.setx(400)
+
+        space.update()
+        time.sleep(0.2)
+        
+
+        if eletszamlalo==0:
+            time.sleep(5)
