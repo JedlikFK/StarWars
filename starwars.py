3f964e6feb40456548f0392c0b13e5638e7b7895
JedlikFK <mobilephp@gmail.com>
2021-03-22 19:34:08 +0100

    laserkesz

--- starwars.py
+++ starwars.py
@@ -1,6 +1,7 @@
 import turtle
 from random import randint 
 import time
+import array as array
 
 def fel():
     ypozicio=urhajo.ycor()
@@ -22,6 +23,12 @@ def balra():
     xpozicio-=10
     urhajo.setx(xpozicio)
 
+def shoot():
+    xpozicio=urhajo.xcor()
+    ypozicio=urhajo.ycor()
+    laser.setx(xpozicio)
+    laser.sety(ypozicio)
+
 kijelzo = turtle.Turtle()
 kijelzo.hideturtle()
 kijelzo.penup()
@@ -40,12 +47,14 @@ space.setup(width=800, height=600)
 space.bgpic("background.png")
 space.addshape("sprite.gif")
 space.addshape("meteor1.gif")
+space.addshape("laser.gif")
 space.tracer(0)
 space.listen()
 space.onkey(fel ,"Up")
 space.onkey(le ,"Down")
 space.onkey(balra ,"Left")
 space.onkey(jobbra ,"Right")
+space.onkey(shoot, "space")
 
 urhajo= turtle.Turtle()
 urhajo.shape("sprite.gif")
@@ -56,26 +65,37 @@ meteor1.shape("meteor1.gif")
 meteor1.penup()
 meteor1.setx(400)
 meteor1.sety(randint(-30,30)*10)
+#meteor1.sety(0)
 
+laser=turtle.Turtle()
+laser.shape("laser.gif")
+laser.penup()
+
+tureshatarok = array.array('i', [ -40,-30, -20, -10, 0, 10, 20, 30, 40,])
+readytoshoot=True
 szamlalo=0
-eletszamlalo=1
+eletszamlalo=10
 while eletszamlalo!=0:
     
 
     while meteor1.xcor() > -400:
         space.update()
-        time.sleep(0.05)
         meteor_mozgas = meteor1.xcor()
-        meteor_mozgas -= 10
+        meteor_mozgas -= 5
+        time.sleep(0.01)
+        
         meteor1.setx(meteor_mozgas)
 
         eletkijelzo.write(f"Életeid: {eletszamlalo}", align="center", font=("Arial", 30, "bold"))
         
-        if meteor1.xcor()==urhajo.xcor() and meteor1.ycor()==urhajo.ycor():
-            eletkijelzo.clear()
-            eletszamlalo-=1
-            meteor1.setx(400)
-            meteor1.sety(randint(-30,30)*10)
+        for t in tureshatarok:
+            if meteor1.xcor()==urhajo.xcor()+t:
+                for t in tureshatarok:
+                    if meteor1.ycor()==urhajo.ycor()+t:
+                        eletkijelzo.clear()
+                        eletszamlalo-=1
+                        meteor1.setx(400)
+                        meteor1.sety(randint(-30,30)*10)
             
 
         if eletszamlalo==0:
@@ -83,14 +103,42 @@ while eletszamlalo!=0:
             kijelzo.write("GAME OVER", align="center", font=("Arial", 60, "bold"))
             urhajo.setx(10000)
             urhajo.sety(10000)
+
+
+        if readytoshoot==True:
+            laser_mozgas=laser.xcor()
+            readytoshoot=False
+            laser_mozgas_ycor=laser.ycor()
+
+        if laser_mozgas>400:
+            readytoshoot=True
+        
+        laser_mozgas+=10
+        laser.setx(laser_mozgas)
+        laser.sety(laser_mozgas_ycor)
+        
+        for t in tureshatarok:
+            if meteor1.xcor()==laser.xcor()+t:
+                for t in tureshatarok:
+                    if meteor1.ycor()==laser.ycor()+t:
+                        kijelzo.clear()
+                        szamlalo+=1
+                        meteor1.setx(400)
+                        meteor1.sety(randint(-30,30)*10)
+                        #meteor1.sety(0)
+                        laser_mozgas=450
+
+        kijelzo.clear()
+        kijelzo.write(f"Pontjaid: {szamlalo}", align="center", font=("Arial", 30, "bold"))       
+
     
     while meteor1.xcor() <= -400:
         
         if meteor1.xcor()==-400:
             meteor1.setx(400)
             meteor1.sety(randint(-30,30)*10)
-            if eletszamlalo!=0:
-                szamlalo+=1
+            #meteor1.sety(0)
+            
 
         kijelzo.clear()
         kijelzo.write(f"Pontjaid: {szamlalo}", align="center", font=("Arial", 30, "bold"))
@@ -108,7 +156,7 @@ while eletszamlalo!=0:
             urhajo.setx(400)
 
         space.update()
-        time.sleep(0.2)
+        time.sleep(0.01)
         
 
         if eletszamlalo==0:
